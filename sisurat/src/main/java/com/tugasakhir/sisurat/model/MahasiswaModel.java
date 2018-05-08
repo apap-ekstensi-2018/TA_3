19d9fa822bcc7b50dd1c9043f1eaea5dc6fd2631
alexfmanihuruk <alexfmanihuruk@gmail.com>
2018-05-08 20:43:48 +0700

    add Mahasiswa model and dao

--- sisurat/src/main/java/com/tugasakhir/sisurat/model/MahasiswaModel.java
+++ sisurat/src/main/java/com/tugasakhir/sisurat/model/MahasiswaModel.java
@@ -1,36 +1,17 @@
 package com.tugasakhir.sisurat.model;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.validation.constraints.Size;
-
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class MahasiswaModel
-{
-	@NotNull
-    private int id;
-	
-	@NotNull
-    private String npm;
-    
-	@NotNull
-	//@Min(1)@Max(4)
-    private String username;
-	
-	@NotNull
-	@Size(min=2, max=30)
-    private String name;
-	
-	@NotNull
-	//@Min(1)@Max(4)
-    private String status;
 
+public class MahasiswaModel {
+	private int id;
+	private String npm;
+	private String username;
+	private String nama;
+	private String status;
 }
