93a28b39fcadb25d71091b9508243b246411e896
lidianatalia <lidiamanik@outlook.com>
2018-05-10 00:20:19 +0700

    rest pegawai

--- sisurat/src/main/java/com/tugasakhir/sisurat/rest/PegawaiRestController.java
+++ sisurat/src/main/java/com/tugasakhir/sisurat/rest/PegawaiRestController.java
@@ -1,5 +1,22 @@
 package com.tugasakhir.sisurat.rest;
 
-public class PegawaiRestController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.tugasakhir.sisurat.model.PegawaiModel;
+import com.tugasakhir.sisurat.service.PegawaiService;
+
+@RestController
+@RequestMapping ("/rest")
+public class PegawaiRestController {
+	@Autowired
+	PegawaiService pegawaiService;
+	
+	@RequestMapping ("pengajuan/riwayat/{nip}")
+	public PegawaiModel viewRiwayat (@PathVariable (value="nip")String nip){
+		PegawaiModel pegawaiModel = pegawaiService.selectPegawai(nip);
+		return pegawaiModel;
+	}
 }
