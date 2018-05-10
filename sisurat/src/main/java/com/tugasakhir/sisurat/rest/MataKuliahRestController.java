2b3892b83d1e90cf0d1ed5ac17c6de51e3bfa640
rahma <rahma.putrianasaricoc@gmail.com>
2018-05-10 15:12:45 +0700

    matakuliah service

--- sisurat/src/main/java/com/tugasakhir/sisurat/rest/MataKuliahRestController.java
+++ sisurat/src/main/java/com/tugasakhir/sisurat/rest/MataKuliahRestController.java
@@ -1,28 +1,22 @@
 package com.tugasakhir.sisurat.rest;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.tugasakhir.sisurat.dao.SuratDAO;
-import com.tugasakhir.sisurat.dao.SuratMapper;
-import com.tugasakhir.sisurat.model.JenisSuratModel;
-import com.tugasakhir.sisurat.model.MataKuliahModel;
-import lombok.extern.slf4j.Slf4j;
+import com.tugasakhir.sisurat.model.MahasiswaModel;
+import com.tugasakhir.sisurat.service.MahasiswaService;
 
-@Slf4j
-@Service
-@Primary
-public class MatakuliahServiceRest {
-	@Autowired
-	private SuratDAO suratDAO;
-	
-//	@Override
-//	public List<MatakuliahModel>selectAllMatakuliah()
-//	{
-//		log.info("Rest - pilih semua matakuliah");
-//		return suratDAO.selectAllMatakuliah();
-//	}
+@RestController
+@RequestMapping("/rest")
+public class MataKuliahRestController {
+ @Autowired
+ MahasiswaService mahasiswaService;
+ 
+ @RequestMapping("pengajuan/riwayat/{npm}")
+ public MahasiswaModel viewRiwayat(@PathVariable(value="npm") String npm) {
+	 MahasiswaModel mahasiswaModel = mahasiswaService.selectMahasiswa(npm);
+	 return mahasiswaModel;
+ }
 }
