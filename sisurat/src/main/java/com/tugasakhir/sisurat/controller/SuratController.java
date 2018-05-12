cf1ee11e3d7619d16a503698807fed0940baefa2
David Elkana <davidelkana@Davids-MacBook-Pro.local>
2018-05-12 23:43:43 +0700

    fix detail riwayat add some kolom data

--- sisurat/src/main/java/com/tugasakhir/sisurat/controller/SuratController.java
+++ sisurat/src/main/java/com/tugasakhir/sisurat/controller/SuratController.java
@@ -20,9 +20,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tugasakhir.sisurat.model.JenisSuratModel;
+import com.tugasakhir.sisurat.model.MahasiswaModel;
+import com.tugasakhir.sisurat.model.MataKuliahModel;
+import com.tugasakhir.sisurat.model.PegawaiModel;
 import com.tugasakhir.sisurat.model.PengajuanSuratModel;
 import com.tugasakhir.sisurat.model.SuratModel;
+import com.tugasakhir.sisurat.service.MahasiswaService;
 import com.tugasakhir.sisurat.service.MahasiswaServiceRest;
+import com.tugasakhir.sisurat.service.MataKuliahService;
+import com.tugasakhir.sisurat.service.PegawaiService;
 import com.tugasakhir.sisurat.service.SuratService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -33,6 +39,15 @@ public class SuratController {
 	@Autowired
 	SuratService suratDAO;
 
+	@Autowired
+	MahasiswaService mahasiswaService;
+
+	@Autowired
+	PegawaiService pegawaiService;
+
+	@Autowired
+	 MataKuliahService mkService;
+	
 	@RequestMapping(value="/pengajuan/tambah", method= RequestMethod.POST)
 	public String addSubmit(@ModelAttribute("pengajuan_surat") PengajuanSuratModel pengajuan_surat, Model model) {
 		if (pengajuan_surat.getAlasan_izin().isEmpty()) {
@@ -78,7 +93,16 @@ public class SuratController {
 	 @RequestMapping("/pengajuan/riwayat/{idSurat}")
 	 public String viewPath (Model model, @PathVariable(value = "idSurat") String idSurat){
 		 PengajuanSuratModel pengajuanSuratModel = suratDAO.selectPengajuan(Integer.parseInt(idSurat));
-		 
+		 MahasiswaModel mahasiswa = mahasiswaService.selectMahasiswa(pengajuanSuratModel.getUsername_pengaju());
+		 pengajuanSuratModel.setPengaju(mahasiswa);
+		 if(pengajuanSuratModel.getId_matkul_terkait()!=null) {
+			 MataKuliahModel mataKuliahModel = mkService.selectMataKuliah(pengajuanSuratModel.getId_matkul_terkait());
+			 pengajuanSuratModel.setMata_kuliah(mataKuliahModel);
+		 }
+		 if(pengajuanSuratModel.getUsername_pegawai()!=null) {
+			 PegawaiModel pegawai = pegawaiService.selectPegawai(pengajuanSuratModel.getUsername_pegawai());
+			 pengajuanSuratModel.setPegawai(pegawai);
+		 }
 		 model.addAttribute("pengajuan_surat", pengajuanSuratModel);
 		 return "pengajuan-riwayat-detail";	    
 	 }
